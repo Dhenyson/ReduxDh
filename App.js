8b779a83df4d029360986029de16632cf1aace70
Dhenyson <dhenysonjhean@gmail.com>
2020-12-03 02:01:14 -0300

    Second commit

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import {StyleSheet, Text} from 'react-native';
 
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import Store from './src/Store.js';
-
-import { connect } from 'react-redux';
 
 const App = (props) => {
   return (
